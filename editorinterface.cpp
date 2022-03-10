a698c9e20bfe2eee694eedad62e8f80a29fe904d
Aliaksandr Skuratovich <skuratovich.aliaksandr@gmail.com>
2022-03-10 15:08:13 +0100

    add buttons

--- editorinterface.cpp
+++ editorinterface.cpp
@@ -1,6 +1,8 @@
 #include "editorinterface.h"
 #include "ui_editorinterface.h"
 
+#include <QMessageBox>
+
 editorInterface::editorInterface(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::editorInterface)
@@ -13,3 +15,121 @@ editorInterface::~editorInterface()
 {
     delete ui;
 }
+
+void editorInterface::on_actionNew_Project_triggered()
+{
+   QMessageBox::information(this, "TODO", "new project");
+}
+
+
+void editorInterface::on_actionOpen_triggered()
+{
+   QMessageBox::information(this, "TODO", "open");
+
+}
+
+
+void editorInterface::on_actionSave_triggered()
+{
+   QMessageBox::information(this, "TODO", "save");
+
+}
+
+
+void editorInterface::on_actionAll_triggered()
+{
+   QMessageBox::information(this, "TODO", "select everything");
+
+}
+
+
+void editorInterface::on_actionAll_Objects_triggered()
+{
+   QMessageBox::information(this, "TODO", "select all objects");
+
+}
+
+
+void editorInterface::on_actionAll_Lines_triggered()
+{
+   QMessageBox::information(this, "TODO", "select all lilnes");
+
+}
+
+
+void editorInterface::on_actionZoom_In_triggered()
+{
+
+   QMessageBox::information(this, "TODO", "zoom in");
+}
+
+
+void editorInterface::on_actionZoom_Out_triggered()
+{
+   QMessageBox::information(this, "TODO", "zoom out");
+
+}
+
+
+void editorInterface::on_actionReset_Zoom_triggered()
+{
+   QMessageBox::information(this, "TODO", "reset zoom   ");
+
+}
+
+
+void editorInterface::on_actionNew_Diagram_triggered()
+{
+   QMessageBox::information(this, "TODO", "new diagram");
+
+}
+
+
+void editorInterface::on_actionImport_diagram_triggered()
+{
+   QMessageBox::information(this, "TODO", "import ");
+
+}
+
+
+void editorInterface::on_actionSave_As_triggered()
+{
+   QMessageBox::information(this, "TODO", "save as");
+
+}
+
+
+void editorInterface::on_actionCopy_triggered()
+{
+   QMessageBox::information(this, "TODO", "copy");
+
+}
+
+
+void editorInterface::on_actionPaste_triggered()
+{
+   QMessageBox::information(this, "TODO", "paste");
+
+}
+
+
+void editorInterface::on_actionCut_triggered()
+{
+   QMessageBox::information(this, "TODO", "cut");
+
+}
+
+
+void editorInterface::on_actionUndo_triggered()
+{
+   QMessageBox::information(this, "TODO", "undo");
+
+}
+
+
+void editorInterface::on_actionRedo_triggered()
+{
+   QMessageBox::information(this, "TODO", "Redo");
+
+}
+
--- editorinterface.h
+++ editorinterface.h
@@ -17,6 +17,41 @@ public:
 
     //void loadFile(const QString &filename);
 
+private slots:
+    void on_actionNew_Project_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionAll_triggered();
+
+    void on_actionAll_Objects_triggered();
+
+    void on_actionAll_Lines_triggered();
+
+    void on_actionZoom_In_triggered();
+
+    void on_actionZoom_Out_triggered();
+
+    void on_actionReset_Zoom_triggered();
+
+    void on_actionNew_Diagram_triggered();
+
+    void on_actionImport_diagram_triggered();
+
+    void on_actionSave_As_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
 private:
     Ui::editorInterface *ui;
 };
