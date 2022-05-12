214f6fbd99b0f2c2fd44036849c1c498067ce253
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2022-05-12 18:31:05 +0200

    parse commented

--- src/editor/Backend/Parse.h
+++ src/editor/Backend/Parse.h
@@ -16,6 +16,9 @@
 
 using json = nlohmann::json;
 
+/**
+ * Program representation in json format.
+ */
 class Program {
 public:
     dgrmClass_t diagramClass;
@@ -23,9 +26,22 @@ public:
     DiagramClass obj_cl;
     DiagramSequence obj_se;
 
+    /**
+     * @brief Read a json file and fill in all structures related to the node of json file.
+     * @param filename name of the file for reading
+     */
     void parseFile(std::string filename);
 
-    static void pushCoords(const json el, std::vector<double>& coords);
+    /**
+     * @brief Set values to the structure coords from json file.
+     * @param el node of a json file with coordinates [x, y] for object
+     * @param coords vector with coordinates
+     */
+    static void pushCoords(const json el, std::vector<double> &coords);
 
+    /**
+     * @brief Fill an array of class_dgrm and seq_dgrm nodes in json file.
+     * @param filename name of the file for writing
+     */
     void fillFile(std::string filename);
 };
